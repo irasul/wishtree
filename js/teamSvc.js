591c9f3d234351ec0b0cf68cd5e30c31d93a1223
Iftakhar Rasul <iftakhar.rasul@gmail.com>
2017-05-19 06:53:17 -0400

    new Service - teamSvc - isolating team details from main js

---
+++ js/teamSvc.js
@@ -0,0 +1,77 @@
+app.service('teamSvc',function(){
+	this.members = {
+		0:{
+			fullname : 'Shayan Mahfuz',
+			firstname: 'Shayan', 
+			title    : 'Founder, Sr. Cinematographer',
+			about    : `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s`,
+			picUrl   : "img/team/s.jpg",
+
+		},
+		1:{
+			fullname : 'Iftakhar Rasul',
+			firstName: 'Iftakhar', 
+			title    : 'Founder, Lead Photographer',
+			about    : `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s`,
+			picUrl   : "img/team/i.jpg",
+
+		},
+		2:{
+			fullname : 'Yaman Syed',
+			firstName: 'Yaman', 
+			title    : 'Sr. Cinematographer',
+			about    : `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s`,
+			picUrl   : "img/team/y.jpg",
+
+		},
+		3:{
+			fullname : 'Ashiqul Alam',
+			firstName: 'Ashiqul', 
+			title    : 'Sr. Cinematographer',
+			about    : `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s`,
+			picUrl   : "img/team/a.jpg",
+
+		},
+		4:{
+			fullname : 'Mahfuz Rahman',
+			firstName: 'Mahfuz', 
+			title    : 'Sr. Photographer',
+			about    : `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s`,
+			picUrl   : "img/team/m.jpg",
+
+		},
+		5:{
+			fullname : 'Omar Ali',
+			firstName: 'Omar', 
+			title    : 'Photographer',
+			about    : `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s`,
+			picUrl   : "img/team/o.jpg",
+
+		},
+		6:{
+			fullname : 'Kaiser Ahmed',
+			firstName: 'Kaiser', 
+			title    : 'Photographer',
+			about    : `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s`,
+			picUrl   : "img/team/k.jpg",
+
+		},
+		7:{
+			fullname : 'Tareq Zilani',
+			firstName: 'Tareq', 
+			title    : 'Videographer',
+			about    : `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s`,
+			picUrl   : "img/team/t.jpg",
+
+		},
+		8:{
+			fullname : 'Shahriyar Ahmed',
+			firstName: 'Shahriyar', 
+			title    : 'Founder, Sr. Cinematographer',
+			about    : `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s`,
+			picUrl   : "img/team/sh.jpg",
+
+		}
+	}
+
+});
--- js/wt.js
+++ js/wt.js
@@ -33,115 +33,8 @@ $(document).ready(function(){
 	
 });
 var app = angular.module('app',[]);
-app.service('packages',function(){
-	this.photo = {
-		title: 'PHOTO PACKAGES',
-		packageNames:{
-			Daily: ['Essential','Elite'],
-			Hourly: ['Essential','Luxury','Royalty','Elite'],
-		},
-		Daily:{
-			Essential:[	
-					"Full Event Coverage", 
-					"1 Photographer", 
-					"Full Content provided on an online interactive platform"
-			],
-			Elite:[
 
-				"Full Day of Coverage",
-				"2 Photographers",
-				"Coverage of Bride and Groom Preparation",
-				"Exclusive Session with Bride and Groom",
-				"Full Content provided on an online interactive platform"
-			]
-		},
-
-		Hourly:{
-			Essential: [
-				"6 hours of Coverage",
-				"1 Photographer",
-				"Full Content provided on an online interactive platform"
-			],
-			Luxury: [
-				"8 hours of Coverage",
-				"2 Photographers",
-				"Full Content provided on an online interactive platform"	
-			],
-			Royalty: [
-				"12 hours of Coverage",
-				"2 Photographers",
-				"Coverage of Bride and Groom Preparation",
-				"Photo Book",
-				"Full Content provided on an online interactive platform"
-			],			
-			Elite: [
-				"16 hours of Coverage",
-				"2 Photographers",
-				"Coverage of Bride and Groom Preparation",
-				"Exclusive Session with Bride and Groom",
-				"Photo Book",
-				"1 Hour E-Shoot",
-				"Full Content provided on an online interactive platform"
-			]
-		}
-	}
-
-	this.video = {
-		title: 'CINEMA PACKAGES',
-		packageNames:{
-			Daily: ['Essential','Elite'],
-			Hourly: ['Essential','Luxury','Royalty','Elite'],
-		},
-		Daily:{
-			Essential:[	
-					"Full Event Coverage", 
-					"1 Cinematographer", 
-					"Full Content provided on an online interactive platform"
-			],
-			Elite:[
-
-				"Full Day of Coverage",
-				"2 Cinematographers",
-				"1 Event Highlight",
-				"Coverage of Bride and Groom Preparation",
-				"Exclusive Session with Bride and Groom",
-				"Drone Shots",
-				"Full Content provided on an online interactive platform"
-			]
-		},
-
-		Hourly:{
-			Essential: [
-				"6 hours of Coverage",
-				"1 Cinematographer",
-				"Full Content provided on an online interactive platform"
-			],			
-			Luxury: [
-				"8 hours of Coverage",
-				"2 Cinematographer",
-				"1 Event Highlight",
-				"Full Content provided on an online interactive platform"	
-			],
-			Royalty: [
-				"12 hours of Coverage",
-				"2 Cinematographer",
-				"1 Event Highlight",
-				"Coverage of Bride and Groom Preparation",
-				"Full Content provided on an online interactive platform"
-			],
-			Elite: [
-				"16 hours of Coverage",
-				"2 Cinematographer",
-				"1 Event Highlight",
-				"Coverage of Bride and Groom Preparation",
-				"Exclusive Session with Bride and Groom",
-				"Drone Shots",
-				"Full Content provided on an online interactive platform"
-			]
-		}
-	}
-});
-app.controller('ctrl',['$scope','packages','$timeout', function($scope,packages,$timeout){
+app.controller('ctrl',['$scope','packages','teamSvc','$timeout', function($scope,packages,teamSvc,$timeout){
 	$scope.mobile = window.mobilecheck();
 	$scope.showcase = 1;
 	$scope.showModal = false;
@@ -163,6 +56,8 @@ app.controller('ctrl',['$scope','packages','$timeout', function($scope,packages,
 	}
 
 	$scope.pkg = packages.photo;
+	$scope.teamMembers = teamSvc.members;
+
 
 }]);
 
