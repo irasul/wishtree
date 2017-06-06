98d2177d59d8631c48739cd1bf77a69cc110baee
irasul <irasul@crosscap.com>
2017-06-06 15:33:22 -0400

    team bio

--- js/teamSvc.js
+++ js/teamSvc.js
@@ -4,7 +4,10 @@ app.service('teamSvc',function(){
 			fullname : 'Shayan Mahfuz',
 			firstname: 'Shayan', 
 			title    : 'Founder, Sr. Cinematographer',
-			about    : `With WishTree since 2014`,
+			about    : `Shayan spearheads the videography team and for him, videography IS life. The Wishtree team is hardworking and they’ve all become like family, and this, he says, is what makes them so great. Everyone who has worked with Shayan knows he has an infectious personality will make any client happy. His superpower wish? Super-strength! so that he could carry all his equipment at once.
+Favourite TV Show- That 70s Show.
+Interesting Fact about Shayan- He is scared of the dark.
+`,
 			picUrl   : "img/team/s.jpg",
 
 		},
@@ -12,7 +15,10 @@ app.service('teamSvc',function(){
 			fullname : 'Iftakhar Rasul',
 			firstName: 'Iftakhar', 
 			title    : 'Founder, Lead Photographer',
-			about    : `With WishTree since 2014`,
+			about    : `When Iftakhar is masterminding photo compositions, he is solving tech problems as a Web Developer. He says what makes WishTree special is that they have provided the opportunity to good photographers and videographers to start a career professionally. Senior members in the team have taken on interns and provided mentorship, which is quite unique. If Iftakhar could have a super power, he would want to fly. He says, “I always thought floating in the mid-air could be an awesome way to release stress and clean your mind. Also I wouldn’t need drones anymore for the aerial shots.”
+Favourite TV Shows- Seinfeld, House, Game of Thrones, House of Cards, Breaking Bad, Dirk Gentley, Suits, Halt and Catch Fire, Sons of Anarchy, Any Marvel Series and list goes on and on.
+Interesting fact about Iftakhar- He drinks orange juice with salt & he loves MacDonald's BBQ sauce!
+`,
 			picUrl   : "img/team/i.jpg",
 
 		},
@@ -28,7 +34,10 @@ app.service('teamSvc',function(){
 			fullname : 'Ashiqul Alam',
 			firstName: 'Ashiqul', 
 			title    : 'Sr. Cinematographer',
-			about    : `With WishTree since 2014`,
+			about    : `Ashiq is a Claims Adjuster and a busy dad of two. He says the unique quality of Wishtree are the fantastic people who make any experience fun for the client.
+Favourite TV Show- Dexter.
+Interesting Fact about Ashiq: His brain sometimes deliberately makes him forget things and it drives him crazy! Interesting indeed…
+`,
 			picUrl   : "img/team/a.jpg",
 
 		},
@@ -44,7 +53,10 @@ app.service('teamSvc',function(){
 			fullname : 'Omar Alsayyed',
 			firstName: 'Omar', 
 			title    : 'Photographer',
-			about    : `With WishTree since 2017`,
+			about    : `The multi-talented Omar is currently enrolled in the Osgoode Hall Law’s J.D. program and is on route to becoming a full-fledged lawyer (aka esquire). He is also working on his own company called Prowler, selling customized belt products. According to Omar, he IS what makes Wishtree special. If he could have any superpower, it would be to a clairvoyant—it would make for a foolproof photoshoot prep.
+Favourite TV Shows- Friends, The Crown
+Interesting Facts about Omar- He is a Drake look-alike… only part-time… only when he shaves… and gets a low fade.
+`,
 			picUrl   : "img/team/placeholder.png",
 
 		},
@@ -52,7 +64,10 @@ app.service('teamSvc',function(){
 			fullname : 'Kaiser Ahmed',
 			firstName: 'Kaiser', 
 			title    : 'Photographer',
-			about    : `With WishTree since 2016`,
+			about    : `Kaiser has a career as a Senior IT Analyst. His ideal superpower would be to have mind reading abilities so he can find out what people really think about him. Kaiser believes the cheerful personalities and professionalism of the photographers and videographers of Wishtree is what makes them truly unique. It certainly makes the client’s (your) life, a whole lot easier.
+Favourite TV Show- Criminal minds – he can watch a season in one seating.
+Interesting Fact about Kaiser- He can go without sleep for a few days… (that can’t be healthy! oh well..)
+`,
 			picUrl   : "img/team/k.jpg",
 
 		},
@@ -60,7 +75,10 @@ app.service('teamSvc',function(){
 			fullname : 'Tareq Mohammed',
 			firstName: 'Tareq', 
 			title    : 'Videographer',
-			about    : `With WishTree since 2017`,
+			about    : `Tareq is a Sales Performance Manager at Avis Budget Group by weekday, and photographer by weekend.  He wishes he could teleport so he could travel the world within split seconds. The team’s creativity and ability to make valuable moments memorable is the core of why Tareq enjoys being a part of the Wishtree family.
+Favourite TV Show- Breaking Bad
+Interesting Fact about Tareq- He loves watching sports.
+`,
 			picUrl   : "img/team/t.jpg",
 
 		},
@@ -68,7 +86,10 @@ app.service('teamSvc',function(){
 			fullname : 'Hasan Shahryer',
 			firstName: 'Shahryar', 
 			title    : 'Videographer',
-			about    : `With WishTree since 2017`,
+			about    : `Hasan is a Computer Science graduate and has a career as a Business Systems Analyst. Wishtree in his opinion is a place where photo enthusiasts gathered to redefine wedding cinematography and photography. The dedication, motivation, eagerness to change and improve shines in their work because they all they do what they love and they love what they do. If Hasan could have any superpower, it would be the power to predict the future so he can protect his family and friends and keep them out of danger.
+Favourite TV Shows- Game Of Thrones, The Big Bang Theory
+Interesting Facts about Hasan- He loves to travel, drive, play the guitar, and sing. He wants to learn to fly a plane and drive racecars.
+`,
 			picUrl   : "img/team/sh.jpg",
 
 		}
