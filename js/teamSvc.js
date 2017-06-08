e46a45ab700130dc329a3733ca8fe5c5a1b72955
irasul <irasul@crosscap.com>
2017-06-08 16:10:26 -0400

    team bio update

--- js/teamSvc.js
+++ js/teamSvc.js
@@ -4,7 +4,9 @@ app.service('teamSvc',function(){
 			fullname : 'Shayan Mahfuz',
 			firstname: 'Shayan', 
 			title    : 'Founder, Sr. Cinematographer',
-			about    : `Shayan is the captain and the main editor of the cinematography team. His ability to tell a story always leave our clients in tears. The Wishtree team is hardworking and they’ve all become like family, and this, he says, is what makes them so great. Everyone who has worked with Shayan knows he has an infectious personality will make any client happy. His superpower wish? Super-strength! so that he could carry all his equipment at once. Favourite TV Show- That 70s Show. Interesting Fact about Shayan- He is scared of the dark.`,
+			about    : `Shayan is the captain of the cinematography team and the main man behind edit table. His hopelessly romantic approach to tell a story can soften anyone's heart. The Wishtree team is hardworking and they’ve all become like family, and this, he says, is what makes them so great. Shayan's infectious personality and uncompromising effort to go beyond to make the clients happy is truly admiring. 
+			His superpower wish? Super-strength! So that he could carry all his equipment at once. 
+			Favourite TV Show- That 70s Show. Interesting Fact about Shayan- He is scared of the dark.`,
 			picUrl   : "img/team/s.jpg",
 
 		},
@@ -22,9 +24,9 @@ Interesting fact about Iftakhar- He drinks orange juice with salt & he loves Mac
 			fullname : 'Yaman Syed',
 			firstName: 'Yaman', 
 			title    : 'Sr. Cinematographer',
-			about    : `Yaman’s is the man behind all the crazy creative shots. He doesn't mind getting down and dirty therefore his cinematography always leaves our clients speechless. What makes Wishtree special in his opinion is the passion that goes into executing every project. Yaman’s ideal superhero is Ironman because he is smart, rich, sarcastic and doesn't wear a cape.
-Favourite TV show- Breaking bad
-Interesting Fact about Yaman- Piano and Camera, he’s obsessed with both.`,
+			about    : `Yaman’s the man behind all the crazy creative shots. He doesn't mind getting down and dirty or climbing a tree and enduring a few war-wounds on the way. And the results are breath-taking angles and eye-popping compositions. His cinematography at times will leave you speechless. What makes Wishtree special in his opinion is the passion that goes into executing every project, and passion breads perfection. 
+			Yaman’s ideal superhero is Ironman because he is smart, rich and sarcastic and doesn't wear a cape. He is a big fan of the show “Breaking bad”. 
+			Yaman is obsessed with both music and videography. He is also an accomplished pianist.`,
 			picUrl   : "img/team/placeholder.png",
 
 		},
@@ -32,9 +34,9 @@ Interesting Fact about Yaman- Piano and Camera, he’s obsessed with both.`,
 			fullname : 'Ashiqul Alam',
 			firstName: 'Ashiqul', 
 			title    : 'Sr. Cinematographer',
-			about    : `Ashiq is a super energetic, power house content creating cinematographer. He says the unique quality of Wishtree Cinemas are the fantastic people who make any experience fun for the client. 
-Favourite TV Show- Dexter. 
-Interesting Fact about Ashiq: His brain sometimes deliberately makes him forget things and it drives him crazy! Interesting indeed…`,
+			about    : `Ashiq is a super energetic, power-house content creator cinematographer. He is a one man army. If you have Ashiq shooting an event by himself, you know you are in good hands. His unique ability of capturing all the important moment always left the team amazed. He feels that the fantastic people at WishTree Cinemas makes any experience fun for team members and the same energy spreads in the their clients. 
+			He likes the TV show “Dexter” and fittingly he is pretty dexterous at his job. 
+			Interestingly, he claims that his brain sometimes deliberately makes him forget things and that drives him crazy! Interesting indeed… doesn’t yours?`,
 			picUrl   : "img/team/a.jpg",
 
 		},
@@ -50,7 +52,9 @@ Interesting Fact about Ashiq: His brain sometimes deliberately makes him forget 
 			fullname : 'Omar Alsayyed',
 			firstName: 'Omar', 
 			title    : 'Photographer',
-			about    : `The multi-talented Omar is the whizz-kid behind all our client’s(your) candid photos. According to Omar, he IS what makes Wishtree special. If he could have any superpower, it would be to a clairvoyant—it would make for a foolproof photoshoot prep. Favourite TV Shows- Friends, The Crown Interesting Facts about Omar- He is a Drake look-alike… only part-time… only when he shaves… and gets a low fade.`,
+			about    : `The multi-talented Omar is the whizz-kid behind the candid photos. Omar very humbly states that it is him what makes WishTree special. Omar in all true sense is an unyielding force of nature. Be it photography, public relation or marketing. He wants to do it all. This master of many trades wishes that if he could have any superpower, it would be to a clairvoyant—it would make for a foolproof photoshoot prep. 
+			He like the TV show “Friends” – explains his friendly nature of course and “The Crown” – because clearly he is the crowned price of WishTree. 
+			An interesting fact: He is a Drake look-alike… only part-time though… only when he shaves… and gets a low fade… urgh.. alright!!`,
 			picUrl   : "img/team/placeholder.png",
 
 		},
@@ -58,9 +62,9 @@ Interesting Fact about Ashiq: His brain sometimes deliberately makes him forget 
 			fullname : 'Kaiser Ahmed',
 			firstName: 'Kaiser', 
 			title    : 'Photographer',
-			about    : `Kaiser’s photography will always leave you speechless, his attention to details and capturing the right moments makes him great at what he does. His ideal superpower would be to have mind reading abilities so he can find out what people really think about him. Kaiser believes the cheerful personalities and professionalism of the photographers and videographers of Wishtree is what makes them truly unique. It certainly makes the client’s (your) life, a whole lot easier. 
-Favourite TV Show- Criminal minds – he can watch a season in one seating. 
-Interesting Fact about Kaiser- He can go without sleep for a few days… (that can’t be healthy! oh well..)`,
+			about    : `Kaiser is a zealous photographer. His attention to details and a keen pair of eyes make him crucial to the team’s end product. His ideal superpower would be to be able to read mind so he could understand out what people really think about his work and his customer service. It is this amazing ability to self-critic is what makes him work extra hard every time he goes out for a shoot.  Kaiser believes the cheerful personalities and professionalism of the photographers and videographers of WishTree is what makes it a truly great team to work for. It certainly makes the client’s life a whole lot easier. 
+			His favorite show is Criminal minds – he can watch a season in one seating. 
+			Interesting Fact about Kaiser- He can go without sleep for a few days… (that can’t be healthy! oh well..)`,
 			picUrl   : "img/team/k.jpg",
 
 		},
