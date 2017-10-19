9b5013ae54e89157786cb484934cf646d961d339
irasul <iftakhar.rasul@gmail.com>
2017-10-19 11:46:06 -0400

    bug fix

--- js/rollerSvc.js
+++ js/rollerSvc.js
@@ -1,12 +1,11 @@
 app.service('rollerSvc',function(){
 	this.bgs = [		
 		"img/roll/wt-14.jpg",
-		"img/roll/wt-18.jpg",
 		"img/roll/wt-3.jpg",
 		"img/roll/wt-19.jpg",		
 		"img/roll/wt-16.jpg",
 		"img/roll/wt-12.jpg",
-		
+		"img/roll/wt-26.jpg",		
 		
 		//"img/roll/wt-11.jpg",
 		//"img/roll/wt-9.jpg",
